7cb6f400a5c96e859ff267ffbb936c4748fedf97
Sijing Yang <Sijing.Yang@amd.com>
2026-02-11 17:54:05 +0800

    [verl] feat: add trust_remote_code arg and compilation_config dict support

--- atom/config.py
+++ atom/config.py
@@ -781,6 +781,8 @@ class Config:
                 self.graph_bs = cuda_graph_sizes
 
     def __post_init__(self):
+        if isinstance(self.compilation_config, dict):
+            self.compilation_config = CompilationConfig(**self.compilation_config)
         # assert os.path.isdir(self.model)
 
         assert 1 <= self.tensor_parallel_size <= 8
