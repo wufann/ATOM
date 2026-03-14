e05e0d2b8fbb48eae65dca2bf79290752d9e18fc
github-action-benchmark <github@users.noreply.github.com>
2026-03-14 10:34:07 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 9567af916d7b6fa8d66607d668eec41464448a8c

---
+++ benchmark-dashboard/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773484446972,
+  "repoUrl": "https://github.com/ROCm/ATOM",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Lingpeng Jin",
+            "username": "valarLip",
+            "email": "103567126+valarLip@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "Lingpeng Jin",
+            "username": "valarLip",
+            "email": "103567126+valarLip@users.noreply.github.com"
+          },
+          "id": "9567af916d7b6fa8d66607d668eec41464448a8c",
+          "message": "perf: remove ATOM_DISABLE_MMAP from benchmark workflow to speed up model loading",
+          "timestamp": "2026-03-14T10:29:47Z",
+          "url": "https://github.com/ROCm/ATOM/commit/9567af916d7b6fa8d66607d668eec41464448a8c"
+        },
+        "date": 1773484446473,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "DeepSeek-R1-0528 0/0 c=128 throughput (tok/s)",
+            "value": 4262.89,
+            "unit": "tok/s"
+          },
+          {
+            "name": "DeepSeek-R1-0528 0/0 c=128 TPOT (ms)",
+            "value": 28.81,
+            "unit": "ms"
+          },
+          {
+            "name": "DeepSeek-R1-0528 0/0 c=128 throughput (tok/s)",
+            "value": 6914.47,
+            "unit": "tok/s"
+          },
+          {
+            "name": "DeepSeek-R1-0528 0/0 c=128 TPOT (ms)",
+            "value": 17.39,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
